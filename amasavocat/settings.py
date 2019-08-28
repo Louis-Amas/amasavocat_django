4e4ae2051072f406fbba227a73c8031e7eab9259
Arconec <arconec@sd-125946.dedibox.fr>
2019-08-28 20:06:19 +0200

    bo

--- amasavocat/settings.py
+++ amasavocat/settings.py
@@ -25,7 +25,7 @@ with open('secret') as f:
     SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "gest_avocat/static/"),
